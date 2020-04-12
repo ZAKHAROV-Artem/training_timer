b49d2574b9dcf14500c64ceb1f1041a4192eae1e
ZAKHAROV-Artem <55618462+ZAKHAROV-Artem@users.noreply.github.com>
2020-04-13 00:29:11 +0300

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,9 +1,11 @@
-let ready, one, two, three, audio, obr, i, timerflag;
+let ready, one, two, three, audio, obr, i, timerflag, persent, time10, time60;
 document.querySelector("html").onclick = function () {
   obr = 60;
   i = 1;
   timerflag = true;
-
+  persent = 0;
+  time10 = false;
+  time60 = true;
   //ready alert------------------------------------------------------------------------
 
   ready = document.querySelector(".ready");
@@ -35,45 +37,60 @@ document.querySelector("html").onclick = function () {
 
   //create recognizer  and speechSynthesisUtterance-------------------------------
 
-  let synth = window.speechSynthesis;
-  let payse = new SpeechSynthesisUtterance("Поставлено на паузу");
-  let play = new SpeechSynthesisUtterance("Продолжили");
-  let relax = new SpeechSynthesisUtterance("Оттдых");
-  let work = new SpeechSynthesisUtterance("Начали");
-  let thank = new SpeechSynthesisUtterance("Спасибо за тренировку");
-  let open = new SpeechSynthesisUtterance("Открываю");
+  let synth = window.speechSynthesis,
+    payse = new SpeechSynthesisUtterance("Поставлено на паузу"),
+    play = new SpeechSynthesisUtterance("Продолжили"),
+    relax = new SpeechSynthesisUtterance("Оттдых"),
+    work = new SpeechSynthesisUtterance("Начали"),
+    thank = new SpeechSynthesisUtterance("Спасибо за тренировку"),
+    open = new SpeechSynthesisUtterance("Открываю");
   //------------------------------------------------------------------------------
 
   //timer-------------------------------------------------------------------------
-
   setTimeout(function () {
     timer();
     function timer() {
       document.querySelector(".timer").innerText = obr;
+      let body = document.querySelector("body"),
+        progress = document.querySelector(".progress");
       obr--;
+      if (time60) {
+        progress.style.width = persent * 1.73 + "%";
+      }
+      if (time10) {
+        progress.style.width = persent * 11.8 + "%";
+      }
+
       if (timerflag) {
         setTimeout(timer, 1000);
+        persent++;
       }
+
       if (i % 2 == 1 && obr == 0) {
-        document.querySelector("body").style.background = "white";
+        persent = 0;
+        time60 = false;
+        time10 = true;
+        body.style.background = "white";
         synth.speak(relax);
         setTimeout(function () {
-          document.querySelector("body").style.background = "#212121";
+          body.style.background = "#212121";
         }, 1500);
         i++;
         obr = 10;
       }
       if (i % 2 == 0 && obr == 0) {
-        document.querySelector("body").style.background = "white";
+        persent = 0;
+        time60 = true;
+        time10 = false;
+        body.style.background = "white";
         synth.speak(work);
         setTimeout(function () {
-          document.querySelector("body").style.background = "#212121";
+          body.style.background = "#212121";
         }, 1500);
         i++;
         obr = 60;
       }
     }
-
     //------------------------------------------------------------------------------------------
 
     //all window opening arrays-----------------------------------------------------------------
